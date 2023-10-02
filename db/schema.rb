c55c2b1a3b093afa920625ca07afa0ce8eb115e9
Alex Ventura <me@alexventura.dev>
2023-10-02 11:38:10 -0600

    Add new config after running the official rails update command.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_030919) do
+ActiveRecord::Schema.define(version: 2023_09_29_172158) do
 
   create_table "links", force: :cascade do |t|
     t.string "name"
